1eb1f5239197e45af532967f514194af663d00ff
edutanaka <edutanakabr@gmail.com>
2016-11-22 17:39:26 -0200

    change melpa repository to melpa stable

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
 (add-to-list 'package-archives
              '("tromey" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa" . "http://stable.melpa.org/packages/") t)
 
 ;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 ;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
